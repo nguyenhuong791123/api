d9ac2f6269c3a9d527035ddfe7e38f47cc0d904a
nguyenhuong791123 <nguyenhuong791123@gmail.com>
2019-12-26 23:01:53 +0900

    excel viewer api

--- urls/excelapi.py
+++ urls/excelapi.py
@@ -23,6 +23,7 @@ def excel_viewer():
     html = 'excel/' + dir + '/viewer.handsontable.html'
     tdir = 'templates/excel/' + dir
     tdir = make_dir_local(tdir)
+    objs = []
 
     try:
         book = openpyxl.load_workbook(path)
@@ -34,47 +35,74 @@ def excel_viewer():
             maxr = len(list(sheet.rows))
             maxc = 0
             sa = []
+            vertical = []
+            horizontal = []
+            arrays = []
             for row in sheet.rows:
                 if rIdx >= (maxr - 1):
                     for r in range(len(row)):
                         if r > 0 and is_variable_exist(EXCEL_FUNC, row[r].value):
                             row[r].value = sum_cols(sheet, r)
 
-                    categories = Reference(sheet, min_col=1, max_col=1, min_row=maxr, max_row=maxr)
-                    datas = Reference(sheet, min_col=2, max_col=(maxc - 1), min_row=2, max_row=(maxr - 1))
-                    chart = write_chart(BarChart(), categories, datas, sheetnames[sIdx], 10)
-                    sheet.add_chart(chart, "B" + str((maxr + 2)))
-                    # chart3d = write_chart(BarChart3D(), categories, datas, sheetnames[sIdx], 10)
-                    # sheet.add_chart(chart3d, "K" + str((maxr + 2)))
+                    print(vertical)
+                    print(horizontal)
+                    print(arrays)
+                    sheetname = sheetnames[sIdx]
+                    obj = {}
+                    obj['sheet'] = sheetname
+                    obj['vertical'] = vertical
+                    obj['horizontal'] = horizontal
+                    obj['datas'] = arrays
+                    objs.append(obj)
+                    pos = 1
+                    # if True:
+                    #     position = str(((maxr+2)*pos + (15*(pos-1))))
+                    #     categories = Reference(sheet, min_col=1, min_row=2, max_row=maxr)
+                    #     datas = Reference(sheet, min_col=2, min_row=1, max_col=(maxc - 1), max_row=(maxr - 1))
+                    #     area = write_chart(AreaChart(), categories, datas, sheetname, 10)
+                    #     sheet.add_chart(area, "B" + position)
+                    #     area3d = write_chart(AreaChart3D(), categories, datas, sheetname, 10)
+                    #     sheet.add_chart(area3d, "K" + position)
+                    #     pos = pos + 1
 
-                    categories =  Reference(sheet, min_col=1, min_row=2, max_row=maxr)
-                    datas =  Reference(sheet, min_col=2, max_col=(maxc - 1), min_row=1, max_row=(maxr - 1))
-                    line = write_chart(LineChart(), categories, datas, sheetnames[sIdx], 10)
-                    sheet.add_chart(line, "B" + str((maxr*2 + 15)))
-                    line3d = write_chart(LineChart3D(), categories, datas, sheetnames[sIdx], 10)
-                    sheet.add_chart(line3d, "K" + str((maxr*2 + 15)))
+                    if True:
+                        position = str(((maxr+2)*pos + (15*(pos-1))))
+                        categories =  Reference(sheet, min_col=1, min_row=2, max_row=maxr)
+                        datas =  Reference(sheet, min_col=2, max_col=(maxc - 1), min_row=1, max_row=(maxr - 1))
+                        line = write_chart(LineChart(), categories, datas, sheetname, 10)
+                        sheet.add_chart(line, "B" + position)
+                        line3d = write_chart(LineChart3D(), categories, datas, sheetname, 10)
+                        sheet.add_chart(line3d, "K" + position)
+                        pos = pos + 1
 
-                    categories = Reference(sheet, min_col=1, min_row=2, max_row=maxr)
-                    datas = Reference(sheet, min_col=2, max_col=(maxc - 1), min_row=1, max_row=(maxr - 1))
-                    pie = write_chart(PieChart(), categories, datas, sheetnames[sIdx], 10)
-                    sheet.add_chart(pie, "B" + str((maxr*3 + (15*2))))
-                    pie3d = write_chart(PieChart3D(), categories, datas, sheetnames[sIdx], 10)
-                    sheet.add_chart(pie3d, "K" + str((maxr*3 + (15*2))))
+                    # if True:
+                    #     position = str(((maxr+2)*pos + (15*(pos-1))))
+                    #     categories = Reference(sheet, min_col=1, max_col=1, min_row=maxr, max_row=maxr)
+                    #     datas = Reference(sheet, min_col=2, max_col=(maxc - 1), min_row=2, max_row=(maxr - 1))
+                    #     chart = write_chart(BarChart(), categories, datas, sheetname, 10)
+                    #     sheet.add_chart(chart, "B" + position)
+                    #     # chart3d = write_chart(BarChart3D(), categories, datas, sheetname, 10)
+                    #     # sheet.add_chart(chart3d, "K" + position)
+                    #     pos = pos + 1
 
-                    categories = Reference(sheet, min_col=1, min_row=2, max_row=maxr)
-                    datas = Reference(sheet, min_col=2, min_row=1, max_col=(maxc - 1), max_row=(maxr - 1))
-                    area = write_chart(AreaChart(), categories, datas, sheetnames[sIdx], 10)
-                    sheet.add_chart(area, "B" + str((maxr*4 + (15*3))))
-                    area3d = write_chart(AreaChart3D(), categories, datas, sheetnames[sIdx], 10)
-                    sheet.add_chart(area3d, "K" + str((maxr*4 + (15*3))))
+                    if True:
+                        position = str(((maxr+2)*pos + (15*(pos-1))))
+                        categories = Reference(sheet, min_col=1, min_row=2, max_row=maxr)
+                        datas = Reference(sheet, min_col=2, max_col=(maxc - 1), min_row=1, max_row=(maxr - 1))
+                        pie = write_chart(PieChart(), categories, datas, sheetname, 10)
+                        sheet.add_chart(pie, "B" + position)
+                        pie3d = write_chart(PieChart3D(), categories, datas, sheetname, 10)
+                        sheet.add_chart(pie3d, "K" + position)
 
                     break
 
                 cIdx = 0
+                maxc = len(row)
+                array = []
                 if rIdx > 0:
-                    maxc = len(row)
                     for cell in row:
                         if cIdx <= 0:
+                            vertical.append(cell.value)
                             cIdx = cIdx + 1
                             continue
 
@@ -83,9 +111,21 @@ def excel_viewer():
                             cell.value = sr
                             sa.append(sr)
                         else:
-                            cell.value = cIdx
+                            cell.value = (cIdx + rIdx)
+                            array.append(cell.value)
+                        cIdx = cIdx + 1
+                    arrays.append(array)
+                else:
+                    for cell in row:
+                        if cIdx <= 0:
+                            cIdx = cIdx + 1
+                            continue
+                        if cIdx == (maxc - 1):
+                            break
+                        horizontal.append(cell.value)
                         cIdx = cIdx + 1
                 rIdx = rIdx + 1
+            sIdx = sIdx + 1
 
         user_src = tdir + '/report_00.xlsx'
         book.save(user_src)
@@ -101,6 +141,13 @@ def excel_viewer():
     except FileNotFoundError as e:
         print("FileNotFoundError:" + str(e))
 
-    html = render_template('excel/viewer.html', excel_viewer=html)
-    # delete_dir(tdir)
+    # objs = json.dumps({ 'items': objs })
+    html = render_template('excel/viewer.html', excel_viewer=html, objs={ 'items': objs })
+    delete_dir(tdir)
     return html
+    # response = make_response()
+    # response.data = open(user_src, 'rb').read()
+    # response.headers['Content-Disposition'] = "attachment; filename=report_00.xlsx"
+    # response.mimetype = 'application/vnd.ms-excel.sheet.macroEnabled.12'
+    # return response
+
--- utils/viewer/chart.py
+++ utils/viewer/chart.py
@@ -10,6 +10,7 @@ def write_chart(chart, categories, datas, title, height):
     if is_empty(title) == False:
         chart.title = title
 
+    # chart.add_data(datas)
     chart.add_data(datas, titles_from_data = True)
     chart.set_categories(categories)
     return chart
